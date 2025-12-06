a20ce3a495a59fd5e34cc4b86f0c63b6a5960bbc
jiseopshin <wltjq1203@icloud.com>
2025-12-07 04:07:50 +0900

    라이브러리 변경

--- src/test/java/com/fivelogic_recreate/member/application/MemberDeleteServiceTest.java
+++ src/test/java/com/fivelogic_recreate/member/application/MemberDeleteServiceTest.java
@@ -15,7 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -51,6 +51,7 @@ class MemberDeleteServiceTest {
     void shouldThrowExceptionWhenDeleteNonExistingMember() {
         MemberDeleteCommand deleteCommand = new MemberDeleteCommand("nonExistingUserId");
 
-        assertThrows(IllegalArgumentException.class, () -> memberDeleteService.delete(deleteCommand));
+        assertThatThrownBy(() -> memberDeleteService.delete(deleteCommand))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
