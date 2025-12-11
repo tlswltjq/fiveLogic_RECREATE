d6ed6737b34527cd89b05aa37faad783ab818649
jiseopshin <wltjq1203@icloud.com>
2025-12-11 21:05:59 +0900

    예외 핸들러 작성

--- src/main/java/com/fivelogic_recreate/member/interfaces/rest/MemberControllerAdvice.java
+++ src/main/java/com/fivelogic_recreate/member/interfaces/rest/MemberControllerAdvice.java
@@ -1,5 +1,29 @@
 package com.fivelogic_recreate.member.interfaces.rest;
 
+import com.fivelogic_recreate.common.error.BusinessException;
+import com.fivelogic_recreate.common.error.ErrorCode;
+import com.fivelogic_recreate.common.rest.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
 public class MemberControllerAdvice {
-    //TODO ErrorResponse와 함께 구현
+    @ExceptionHandler(BusinessException.class)
+    protected ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
+        log.error("handleBusinessException", e);
+        ErrorCode errorCode = e.getErrorCode();
+        ErrorResponse response = ErrorResponse.of(errorCode);
+        return new ResponseEntity<>(response, errorCode.getStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("handleException", e);
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
