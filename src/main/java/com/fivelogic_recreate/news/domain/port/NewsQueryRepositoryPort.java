9f7cc0258e43a64ffac2709e460b9dcecbc5f6a5
jiseopshin <wltjq1203@icloud.com>
2025-12-26 20:06:03 +0900

    도메인 포트 수정, 읽기 목적이니 List->Page로 변경

--- src/main/java/com/fivelogic_recreate/news/domain/port/NewsQueryRepositoryPort.java
+++ src/main/java/com/fivelogic_recreate/news/domain/port/NewsQueryRepositoryPort.java
@@ -5,16 +5,23 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 public interface NewsQueryRepositoryPort {
     Optional<NewsQueryResponse> findQueryById(Long id);
-    List<NewsQueryResponse> findByTitle(String title);
-    List<NewsQueryResponse> findByContent(String textContent);
-    List<NewsQueryResponse> findByAuthorId(String authorId);
-    List<NewsQueryResponse> findByNewsStatus(String newsStatus);
+
+    Page<NewsQueryResponse> findByTitle(String title, Pageable pageable);
+
+    Page<NewsQueryResponse> findByContent(String textContent, Pageable pageable);
+
+    Page<NewsQueryResponse> findByAuthorId(String authorId, Pageable pageable);
+
+    Page<NewsQueryResponse> findByNewsStatus(String newsStatus, Pageable pageable);
+
     Page<NewsQueryResponse> findByPublishedDateAfter(LocalDateTime publishedDate, Pageable pageable);
+
     Page<NewsQueryResponse> findByPublishedDateBefore(LocalDateTime publishedDate, Pageable pageable);
-    Page<NewsQueryResponse> findByPublishedDateBetween(LocalDateTime startDate, LocalDateTime endDate, Pageable pageable);
+
+    Page<NewsQueryResponse> findByPublishedDateBetween(LocalDateTime startDate, LocalDateTime endDate,
+                                                       Pageable pageable);
 }
