9a4149b9de0d058219dadc76bb5844ef2482cbe3
jiseopshin <wltjq1203@icloud.com>
2025-12-26 20:08:02 +0900

    파사드 구성

--- src/main/java/com/fivelogic_recreate/news/application/NewsService.java
+++ src/main/java/com/fivelogic_recreate/news/application/NewsService.java
@@ -1,12 +1,15 @@
 package com.fivelogic_recreate.news.application;
 
-
 import com.fivelogic_recreate.news.application.command.*;
-import com.fivelogic_recreate.news.application.command.dto.NewsCreateCommand;
-import com.fivelogic_recreate.news.application.command.dto.NewsCreateResult;
+import com.fivelogic_recreate.news.application.command.dto.*;
 import com.fivelogic_recreate.news.application.query.NewsQueryService;
+import com.fivelogic_recreate.news.application.query.dto.NewsQueryResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +19,42 @@ public class NewsService {
     private final NewsHideService newsHideService;
     private final NewsPublishService newsPublishService;
     private final NewsUnHideService newsUnHideService;
+    private final NewsUpdateService newsUpdateService;
     private final NewsQueryService newsQueryService;
 
+    public NewsCreateResult createNews(NewsCreateCommand command) {
+        return newsCreateService.createNews(command);
+    }
+
+    public NewsDeleteResult deleteNews(NewsDeleteCommand command) {
+        return newsDeleteService.deleteNews(command);
+    }
+
+    public NewsHideResult hideNews(NewsHideCommand command) {
+        return newsHideService.hideNews(command);
+    }
+
+    public NewsPublishResult publishNews(NewsPublishCommand command) {
+        return newsPublishService.publishNews(command);
+    }
+
+    public NewsHideResult unhideNews(NewsUnHideCommand command) {
+        return newsUnHideService.unHideNews(command);
+    }
+
+    public NewsUpdateResult updateNews(NewsUpdateCommand command) {
+        return newsUpdateService.updateNews(command);
+    }
+
+    public NewsQueryResponse getNews(Long newsId) {
+        return newsQueryService.findById(newsId);
+    }
+
+    public Page<NewsQueryResponse> getNewsList(Pageable pageable) {
+        return newsQueryService.findByPublishedDateBefore(LocalDateTime.now(), pageable);
+    }
+
+    public Page<NewsQueryResponse> getNewsListByAuthor(String authorId, Pageable pageable) {
+        return newsQueryService.findByAuthorId(authorId, pageable);
+    }
 }
