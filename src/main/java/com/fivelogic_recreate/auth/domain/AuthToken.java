c84ae1ec19fd70c4ae38671628cd66c95a08d72e
jiseopshin <wltjq1203@icloud.com>
2025-12-29 03:25:06 +0900

    토큰 필드 수정

--- src/main/java/com/fivelogic_recreate/auth/domain/AuthToken.java
+++ src/main/java/com/fivelogic_recreate/auth/domain/AuthToken.java
@@ -1,8 +1,10 @@
 package com.fivelogic_recreate.auth.domain;
 
 public record AuthToken(
+        Long memberId,
         String accessToken,
         String refreshToken,
         String grantType,
-        Long expiresIn) {
+        Long expiresIn
+) {
 }
