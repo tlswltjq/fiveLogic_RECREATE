64b4f3e8d015afbcfeb609313973ebd8cb127777
jiseopshin <wltjq1203@icloud.com>
2026-01-12 21:30:51 +0900

    AuthDomainService

--- src/main/java/com/fivelogic_recreate/auth/domain/port/TokenProvider.java
+++ src/main/java/com/fivelogic_recreate/auth/domain/port/TokenProvider.java
@@ -1,6 +1,5 @@
 package com.fivelogic_recreate.auth.domain.port;
 
-import com.fivelogic_recreate.auth.domain.model.AuthUserId;
 import com.fivelogic_recreate.auth.domain.model.Expiration;
 
 public interface TokenProvider {
